7124e2e5e6923ada4aa046855fd03bd983dd9d50
Zaighum Abbas <zaighumabbas786000@gmail.com>
2025-07-09 14:11:20 +0500

    Remove messages.jsx file error

--- frontend/src/components/messages/messages.jsx
+++ frontend/src/components/messages/messages.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 // import Message from "./Message";
-  import Message from ',/message';
+  import Message from './message';
 import useListenMessages from "../../hooks/useListenMessages";
 // import useConversation from "../../../zustand/useConversation";
 
